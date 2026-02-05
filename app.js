5b3bc5e823d59a84b4d17d21823f495ff23505b4
Khieu Dinh Doanh <kd03102004@gmail.com>
2026-02-06 00:33:21 +0700

    run app

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-console.log("Hello, World")
-console.log("Doanhbino")
+const express = require("express");
+
+const app = express();
+const PORT = 8080;
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
+
+app.get("/products", (req, res) => {
+    res.send("Hello World products");
+})
+
+app.listen(PORT, () => {
+    console.log(`My app is running on port 8080: ${PORT} `)
+})
